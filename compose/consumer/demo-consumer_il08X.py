ca561c6c8cc8603880b2d9c49080ac6b6358300a
Christoph Schranz <christoph.schranz@salzburgresearch.at>
2018-07-18 10:13:22 +0200

    prods and cons for the kafka-medium stack

---
+++ compose/consumer/demo-consumer_il08X.py
@@ -0,0 +1,33 @@
+import time
+import json
+# from kafka import KafkaConsumer
+# sudo apt-get install librdkafka-dev python-dev
+from confluent_kafka import Consumer, KafkaError
+
+# TODO create an adapter in python for polling data
+# based on:
+# channelID
+# source own company name
+
+
+KAFKA_TOPIC_IN = "test"
+
+conf = {'bootstrap.servers': 'il081:9093,il082:9094,il083:9095',
+        'group.id': 'mygroup',
+        'default.topic.config': {'auto.offset.reset': 'smallest'}}
+
+consumer = Consumer(**conf)
+consumer.subscribe([KAFKA_TOPIC_IN])
+
+print("Starting to poll")
+
+running = True
+while running:
+    msg = consumer.poll(10)  # in ms
+    if not msg.error():
+        print('Received message: %s' % msg.value().decode('utf-8'))
+    elif msg.error().code() != KafkaError._PARTITION_EOF:
+        print(msg.error())
+        running = False
+
+consumer.close()

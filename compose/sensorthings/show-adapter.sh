0d3339994784da8968df427b21658094ac484812
ChristophSchranz <christoph.schranz@salzburgresearch.at>
2018-09-27 16:05:56 +0200

    Added show state and tried to mount volume

---
+++ compose/sensorthings/show-adapter.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+echo "Printing 'docker service ls | grep st_':"
+docker service ls | grep st
+echo ""
+echo "Printing 'docker stack ps st_gost-db':"
+docker stack ps st_gost-db
+
--- compose/sensorthings/start-sensorthings.sh
+++ compose/sensorthings/start-sensorthings.sh
@@ -2,5 +2,3 @@
 docker-compose build
 docker-compose push || true
 docker stack deploy --compose-file docker-compose.yml st
-
-

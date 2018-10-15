22d7e8e9b7fb5042abf7b64238839ac87c17e607
ChristophSchranz <christoph.schranz@salzburgresearch.at>
2018-10-15 13:17:43 +0200

    showing services, es=2

--- compose/sensorthings/show.sh
+++ compose/sensorthings/show.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 echo "Printing 'docker service ls | grep st_':"
-docker service ls | grep st
+docker service ls | grep st_
 echo ""
 echo "Printing 'docker stack ps st_gost-db':"
 docker stack ps st_gost-db
-
